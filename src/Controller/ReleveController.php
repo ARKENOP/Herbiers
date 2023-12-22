16f2a83c0861dfa923bb9c6aaf6bd1ccc5e40022
Paul SUPIOT <psupiot44@gmail.com>
2023-12-22 16:57:45 +0100

    ajout du CRUD pour Releve

--- src/Controller/ReleveController.php
+++ src/Controller/ReleveController.php
@@ -2,17 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Releve;
+use App\Form\ReleveType;
+use App\Repository\ReleveRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/releve')]
 class ReleveController extends AbstractController
 {
-    #[Route('/releve', name: 'app_releve')]
-    public function index(): Response
+    #[Route('/', name: 'app_releve_index', methods: ['GET'])]
+    public function index(ReleveRepository $releveRepository): Response
     {
+
         return $this->render('releve/index.html.twig', [
-            'controller_name' => 'ReleveController',
+            'releve' => $releveRepository->findAll(),
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_releve_voir', methods: ['GET'])]
+    public function show(Releve $releve): Response
+    {
+        return $this->render('releve/voir.html.twig', [
+            'releve' => $releve,
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'app_releve_ajouter', methods: ['GET', 'POST'])]
+    public function ajouter(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $releve = new Releve();
+
+        $form = $this->createForm(ReleveType::class, $releve);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->persist($releve);
+            $em->flush();
+            return $this->redirectToRoute('app_releve_index');
+        }
+
+        return $this->render('releve/ajouter.html.twig', [
+            'formulaire' => $form->createView()
+        ]);
+    }
+
+    #[Route('/{id}/modifier', name: 'app_releve_modifier', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Releve $releve, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ReleveType::class, $releve);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_releve_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('releve/modifier.html.twig', [
+            'releve' => $releve,
+            'formulaire' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_releve_supprimer', methods: ['POST'])]
+    public function delete(Request $request, Releve $releve, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('supprimer'.$releve->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($releve);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_releve_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/ReleveType.php
+++ src/Form/ReleveType.php
@@ -6,6 +6,8 @@ use App\Entity\Lieu;
 use App\Entity\Releve;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +16,16 @@ class ReleveType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date')
+            ->add('date', DateType::class,[
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ])
             ->add('releveBrut')
             ->add('lieu', EntityType::class, [
                 'class' => Lieu::class,
-'choice_label' => 'id',
+                'choice_label' => 'nom',
             ])
+            ->add('OK', SubmitType::class ,['label' => 'Ajouter'])
         ;
     }
 
